e4628a8956c68ba9c71761f1dbd847dd775af32d
Ethan <git config --global user.email Ethanmbyrd@gmail.com git config --global user.email>
2020-06-22 16:55:03 -0400

    adding more components, favicon, home page

---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+export default function Home() {
+  return (
+    <div className="homeBox">
+      <div className="homeContent">
+        <div className="homeTitlePrimary">Software Developer</div>
+        <div className="homeTitleSecondary">
+          <span id='first'> Ethan</span> <span id='middle'> M. </span> <span id='last'> Byrd </span>
+        </div>
+      </div>
+    </div>
+  );
+}
